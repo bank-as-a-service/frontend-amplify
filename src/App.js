e2e057233ae1352cdb9ef7c2cf6db731733ac68a
LM Bibera <hack.of.all.codes@gmail.com>
2023-03-14 18:27:44 +0800

    BANK-9: Getting close to fixing that issue with the test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Amplify } from 'aws-amplify';
-
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from './App';
 
-test('renders learn react link', () => {
+test('loads and the sign in button as active', async () => {
+  // ARRANGE
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  // ACT
+  await screen.findByRole('tab', { selected: true })
+
+  // ASSERT
+  expect(screen.getByRole('tab', { selected: true })).toHaveTextContent('Sign In')
+})
