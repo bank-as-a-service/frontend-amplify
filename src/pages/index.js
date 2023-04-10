f19b557cbc7b458b2ffe0c6190f78e5f16c0eb3a
LM Bibera <hack.of.all.codes@gmail.com>
2023-04-10 09:46:08 +0800

    BANK-14: Attempt to fix an issue with image

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,11 @@ export default function Home({ formattedDate }) {
         </Head>
         <h1>Static page</h1>
         <Image
-            src="/images/profile.png" // Route of the image file
-            height={144} // Desired size with correct aspect ratio
-            width={144} // Desired size with correct aspect ratio
+            src="/images/profile.png"
+            placeholder="blur"
+            blurDataURL="data:application/xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48RXJyb3I+PENvZGU+SHR0cFZlcnNpb25Ob3RTdXBwb3J0ZWQ8L0NvZGU+PE1lc3NhZ2U+VGhlIEhUVFAgdmVyc2lvbiBzcGVjaWZpZWQgaXMgbm90IHN1cHBvcnRlZC48L01lc3NhZ2U+PFJlcXVlc3RJZD43MkQ4NUVCQkMxQjg3QUVGPC9SZXF1ZXN0SWQ+PEhvc3RJZD5FdWxFc05sTWVLYnBHNStSVlc1bWFFTWlENzJNQ1pCTW8zbytGWmJuVnBYVVJrV1RQZkxoZC9iSWpoa0pUWDJ3czBOSVJQQVcyNGY1U3BwdUNEVkQwK25qQVkvbDNsVDQ8L0hvc3RJZD48L0Vycm9yPg=="
+            height={144}
+            width={144}
             alt="Your Name"
         />
         <p>This page is static. It was built on {formattedDate}.</p>
